b4016ef2512f1341320a6782e531d8ce38e73ad8
vinit <vinit@LAPTOP-V0K7N6A5>
2019-11-26 09:48:38 -0500

    PlaylistDAO

--- src/main/java/empiricist/database/PlaylistsDAO.java
+++ src/main/java/empiricist/database/PlaylistsDAO.java
@@ -2,6 +2,9 @@ package empiricist.database;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import empiricist.model.Playlist;
 
@@ -33,7 +36,97 @@ public class PlaylistsDAO {
             ps.close();
             
             return playlist;
+	    	}
+	    	catch (Exception e) {
+	        	e.printStackTrace();
+	            throw new Exception("Cannot get playlist: " + e.getMessage());
+	        }
 	    }
+	    
+	    public boolean updatePlaylist(Playlist playlist) throws Exception {
+	        try {
+	        	String query = "UPDATE playlist SET value=? WHERE name=?;";
+	        	PreparedStatement ps = conn.prepareStatement(query);
+	            //ps.setDouble(1, playlist.value);
+	            ps.setString(2, playlist.name);
+	            int numAffected = ps.executeUpdate();
+	            ps.close();
+	            
+	            return (numAffected == 1);
+	        } catch (Exception e) {
+	            throw new Exception("Failed to update report: " + e.getMessage());
+	        }
 	    }
+	    
+	    public boolean deletePlaylist(Playlist playlist) throws Exception {
+	        try {
+	            PreparedStatement ps = conn.prepareStatement("DELETE FROM playlist WHERE name = ?;");
+	            ps.setString(1, playlist.name);
+	            int numAffected = ps.executeUpdate();
+	            ps.close();
+	            
+	            return (numAffected == 1);
+
+	        } 
+	        
+	        catch (Exception e) {
+	            throw new Exception("Failed to insert playlist: " + e.getMessage());
+	        }
 	    }
+
+
+	    public boolean addPlaylist(Playlist playlist) throws Exception {
+	        try {
+	            PreparedStatement ps = conn.prepareStatement("SELECT * FROM playlist WHERE name = ?;");
+	            ps.setString(1, playlist.name);
+	            ResultSet resultSet = ps.executeQuery();
+	            
+	            // already present?
+	            while (resultSet.next()) {
+	                Playlist c = generatePlaylist(resultSet);
+	                resultSet.close();
+	                return false;
+	            }
+
+	            ps = conn.prepareStatement("INSERT INTO playlist (name,value) values(?,?);");
+	            ps.setString(1,  playlist.name);
+	            
+	            ps.execute();
+	            return true;
+
+	        } catch (Exception e) {
+	            throw new Exception("Failed to insert constant: " + e.getMessage());
+	        }
+	    }
+
+	    public List<Playlist> getAllPlaylists() throws Exception {
+	        
+	        List<Playlist> allConstants = new ArrayList<>();
+	        try {
+	            Statement statement = conn.createStatement();
+	            String query = "SELECT * FROM playlist";
+	            ResultSet resultSet = statement.executeQuery(query);
+
+	            while (resultSet.next()) {
+	                Playlist p = generatePlaylist(resultSet);
+	                allConstants.add(p);
+	            }
+	            resultSet.close();
+	            statement.close();
+	            return allConstants;
+
+	        } catch (Exception e) {
+	            throw new Exception("Failed in getting playlist: " + e.getMessage());
+	        }
+	    }
+	    
+	    private Playlist generatePlaylist(ResultSet resultSet) throws Exception {
+	        String name  = resultSet.getString("name");
+	        Arraylist<Segment>segment = resultSet.getList("videoSegments");
+	        //Double value = resultSet.getDouble("value");
+	        return new Playlist(name,segment);
+	    }
+
+	}
+	    
 
