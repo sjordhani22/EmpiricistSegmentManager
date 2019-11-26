1483b83840c13356722b89d312816b63207ab01f
Shannon <Shannon@DESKTOP-2UMK8OH>
2019-11-26 13:25:52 -0500

    Worked on Javascript for getSegments- still working on it

--- js/getSegments.js
+++ js/getSegments.js
@@ -11,17 +11,46 @@ function refreshConstantsList() {
    
    console.log("sent");
 
-  // This will process results and update HTML as appropriate. 
-  xhr.onloadend = function () {
+   // This will process results and update HTML as appropriate. 
+   xhr.onloadend = function () {
+     if (xhr.readyState == XMLHttpRequest.DONE) {
+       console.log ("XHR:" + xhr.responseText);
+       processListResponse(xhr.responseText);
+     } else {
+       processListResponse("N/A");
+     }
+   };
+ }
+
+/**
+ * Respond to server JSON object.
+ *
+ * Replace the contents of 'constantList' with a <br>-separated list of name,value pairs.
+ */
+
+function processListResponse(result) {
+	  console.log("res:" + result);
+	  // Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
+	  var js = JSON.parse(result);
+	  var segList = document.getElementById('segList');
 	  
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-    	
-      console.log ("XHR:" + xhr.responseText);
-      processListResponse(xhr.responseText);
-      
-    } else {
-      processListResponse("N/A");
-    }
-  };
+	  var output = "";
+	  for (var i = 0; i < js.list.length; i++) {
+	    var constantJson = js.list[i];
+	    console.log(constantJson);
+	    
+	    var sid = constantJson["id"];
+	    var sname = constantJson["name"];
+	    var squote = constantJson["quote"];
+	    var saddress = constantJson["address"];
+	    var sysvar = constantJson["system"];
+	    if (sysvar) {
+	    	output = output + "<div id=\"const" + sid + "\"><b>" + sname + "\"><b>" + squote +":</b> = " + caddress + "<br></div>";
+	    } else {
+	    	output = output + "<div id=\"const" + sid + "\"><b>" + sname + "\"><b>" + squote + ":</b> = " + caddress + "(<a href='javaScript:requestDelete(\"" + cname + "\")'><img src='deleteIcon.png'></img></a>) <br></div>";
+	    }
+	  }
+	// Update computation result
+	constList.innerHTML = output;
 }
 
