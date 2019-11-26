726edd6f20ec52283a631ab30bb4449bd30c9dbb
jerryperez <jerryperez@localhost>
2019-11-26 14:43:05 -0500

    Changes to getSegments js. JP

--- js/getSegments.js
+++ js/getSegments.js
@@ -1,62 +1,65 @@
 /**
  * Refresh constant list from server
- *
- *    GET list_url
- *    RESPONSE  list of [name, value] constants 
- *    
- *    
- *    
- *    <video id="allFull" controls="" width="320" height="240">
-  <source src="startrek.ogg" type="video/ogg">
-Your browser does not support the video tag.
-</video>
+ * 
+ * GET list_url RESPONSE list of [name, value] constants
+ * 
+ * 
+ * 
+ * <video id="allFull" controls="" width="320" height="240"> <source
+ * src="startrek.ogg" type="video/ogg"> Your browser does not support the video
+ * tag. </video>
  */
-function refreshSegmentsList() {  
-   var xhr = new XMLHttpRequest();  
-   xhr.open("GET", segment_url, true);
-   xhr.send();
-   
-   console.log("sent");
+function refreshSegmentsList() {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", segment_url, true);
+	xhr.send();
 
-   // This will process results and update HTML as appropriate. 
-   xhr.onloadend = function () {
-     if (xhr.readyState == XMLHttpRequest.DONE) {
-       console.log ("XHR:" + xhr.responseText);
-       processListResponse(xhr.responseText);
-     } else {
-       processListResponse("N/A");
-     }
-   };
- }
+	console.log("sent");
+
+	// This will process results and update HTML as appropriate.
+	xhr.onloadend = function() {
+		console.log(xhr);
+		console.log(xhr.request);
+		if (xhr.readyState == XMLHttpRequest.DONE) {
+			processAllSegmentsResponse(xhr.responseText);
+		} else {
+			processAllSegmentsResponse("N/A");
+		}
+	};
+}
 
 /**
  * Respond to server JSON object.
- *
- * Replace the contents of 'constantList' with a <br>-separated list of name,value pairs.
+ * 
+ * Replace the contents of 'constantList' with a <br>
+ * -separated list of name,value pairs.
  */
 
-function processListResponse(result) {
-	  console.log("res:" + result);
-	  // Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
-	  var js = JSON.parse(result);
-	  var segList = document.getElementById('id');
-	  
-	  var output = "";
-	  for (var i = 0; i < js.list.length; i++) {
-	    var constantJson = js.list[i];
-	    console.log(constantJson);
-	    
-	    var sname = constantJson["name"];
-	    var squote = constantJson["quote"];
-	    var saddress = constantJson["address"];
-	    var sysvar = constantJson["system"];
-	    if (sysvar) {
-	    	output = output + "<div id=\"const" + sid + "\"><b>" + sname + "\"><b>" + squote +":</b> = " + caddress + "<br></div>";
-	    } else {
-	    	output = output + "<div id=\"const" + sid + "\"><b>" + sname + "\"><b>" + squote + ":</b> = " + caddress + "(<a href='javaScript:requestDelete(\"" + cname + "\")'><img src='deleteIcon.png'></img></a>) <br></div>";
-	    }
-	  }
-	// Update computation result
-	constList.innerHTML = output;
-}
+function processAllSegmentsResponse(result) {
+	console.log("res:" + result);
+	var js = JSON.parse(result);
+	var segList = document.getElementById('id');
 
+	for (segment in js.videoSegments) {
+		
+		var title = document.createElement('p');
+		var cre = document.createElement('cre');
+		var character = document.createElement('name');
+		var video = document.createElement('address');
+		var source = document.createElement('address');
+		segment = json.videoSegments[segment];
+		title.innerHTML = "Sentence: " + segment['quote'];
+		character.innerHTML = "Character: " + segment['name'];
+		source.type = 'video/ogg';
+		source.src = segment['address'];
+		video.width = 320;
+		video.height = 240;
+		video.controls = true;
+		video.appendChild(source);
+		cre.appendChild(title);
+		cre.appendChild(character);
+		cre.appendChild(video);
+		seglist.appendChild(cre);
+
+	}
+}
