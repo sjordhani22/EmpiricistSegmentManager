84259c68fcdd49d8e8243400b64ee931174a0aa7
jerryperez <jerryperez@localhost>
2019-11-26 08:19:02 -0500

    Office Hours Test -JP

---
+++ src/test/java/handler/TestThings.java
@@ -0,0 +1,29 @@
+package handler;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.Test;
+
+import empiricist.database.SegmentsDAO;
+import empiricist.model.Segment;
+
+public class TestThings {
+
+	@Test
+	public void testFind() {
+	    SegmentsDAO sd = new SegmentsDAO();
+	    try {
+	    	List<Segment> lists = sd.getAllSegments();
+	    	for (Segment s : lists) {
+	    		System.out.println(s);
+	    	}
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    	fail ("didn't work:" + e.getMessage());
+	    }
+	}
+	
+}
