ce9ef70f0d70ce91e3a0e853111b3603cfa69a12
vinit <vinit@LAPTOP-V0K7N6A5>
2019-12-07 17:18:01 -0500

    RegisterSiteRequest -vk

--- src/main/java/empiricist/http/RegisterSiteRequest.java
+++ src/main/java/empiricist/http/RegisterSiteRequest.java
@@ -2,4 +2,29 @@ package empiricist.http;
 
 public class RegisterSiteRequest {
 
+String Url;
+
+public void setUrl(String url) {
+	this.Url = url;
+	
+}
+
+public String getUrl() {
+	
+	return this.Url;
+
+}
+
+public RegisterSiteRequest(String Url) {
+	
+	this.Url = Url;
+	
+	
+}
+
+public String toString() {
+	
+	return "Register(" + Url + ")";
+}
+
 }
