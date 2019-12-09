a63f90588a9c3c37ed77765232b3a2bcf34fc723
jerryperez <jerryperez@autoreg-172150.dyn.wpi.edu>
2019-12-09 14:24:07 -0500

    updated js and html -JP

--- js/CreatePlaylist.js
+++ js/CreatePlaylist.js
@@ -6,47 +6,45 @@ function processCreatePlaylistResponse(result) {
   console.log("result:" + result);
 
   refreshSegmentsList();
-  //refreshRemoteList();														// COMMENT IN EVENTUALLY
+  // refreshRemoteList(); // COMMENT IN EVENTUALLY
 }
 
-function handleCreatePlaylistClick(e) {											//gets called from html
-  var form = document.createPlaylist											//form name from html
- 
-  var data = {};
-  data["name"] = form.name.value;												//inputs from html- get the value from input "name"
+function handleCreatePlaylistClick(e) {											// gets
+																				// called
+																				// from
+																				// html
+	
+  var userIn = document.getElementById("playName");											// form
+																							// name
+																							// from
+																							// html
+  console.log(userIn);
+  var playListName = userIn.value;
+  console.log(playListName);
   
-  if (form.system.checked) {  // be sure to flag system constant requests...
-     data["system"] = true;
-  }
-  
-  // base64EncodedValue":"data:text/plain;base64,My4xND....."
-  var segments = document.createForm.base64Encoding.value.split(',');
-  data["base64EncodedValue"] = segments[1];  // skip first one 
-
-  var js = JSON.stringify(data);
-  console.log("JS:" + js);
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST",createPlaylist_url, true);									// corresponds to API
-
-  // send the collected data as JSON
-  xhr.send(js);
-
-  // This will process results and update HTML as appropriate. 
-  xhr.onloadend = function () {
-    console.log(xhr);
-    console.log(xhr.request);
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-    	 if (xhr.status == 200) {
-	      console.log ("XHR:" + xhr.responseText);
-	      processCreateResponse(xhr.responseText);
-    	 } else {
-    		 console.log("actual:" + xhr.responseText)
-			  var js = JSON.parse(xhr.responseText);
-			  var err = js["response"];
-			  alert (err);
-    	 }
-    } else {
-      processCreatePlaylistResponse("N/A");
-    }
+  if (playListName != ""){
+	  
+	  var data = {};
+	  data{"name"} = playListName;
+	  var xhr = new XMLHttpRequest();
+	  xhr.open("POST",createPlaylist_url,data);
+	  xhr.send();
+	  console.log("sent Playlist data");
+	// This will process results and update HTML as appropriate.
+	  xhr.onloadend = function (){
+		  if(xhr.readyState == XMLHttpRequest.DONE){
+			  if(xhr.status == 200){
+				  console.log("This is what we're getting: " + xhr.responseText);
+				  processCreatePlaylistResponse(xhr.responseText);
+			  }else{
+				  console.log("Actual Response" + xhr.responseText);
+				  var js = JSON.parse(xhr.responseText);
+				  var err = js["response"];
+				  alert (err);
+			  }
+		  }else{
+			  processCreatePlaylistResponse("N/A");
+		  }
+	  }
   };
 }
