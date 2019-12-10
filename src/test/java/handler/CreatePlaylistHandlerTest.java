88e2dc322c53c8e24da0f7c4fa46a37edcde47df
vinit <vinit@LAPTOP-V0K7N6A5>
2019-12-10 13:59:30 -0500

    Test

--- src/test/java/handler/CreatePlaylistHandlerTest.java
+++ src/test/java/handler/CreatePlaylistHandlerTest.java
@@ -3,6 +3,17 @@ package handler;
 //import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import com.google.gson.Gson;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+//import edu.wpi.cs.heineman.calculator.http.AddRequest;
+import empiricist.http.CreatePlayListRequest;
+import empiricist.http.CreatePlayListResponse;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -23,48 +34,41 @@ import com.amazonaws.services.s3.model.S3Object;
 /**
  * A simple test harness for locally invoking your Lambda function handler.
  */
-//@RunWith(MockitoJUnitRunner.class)
-public class CreatePlaylistHandlerTest {
-//
-//    private final String CONTENT_TYPE = "image/jpeg";
-//    private S3Event event;
-//
-//    @Mock
-//    private AmazonS3 s3Client;
-//    @Mock
-//    private S3Object s3Object;
-//
-//    @Captor
-//    private ArgumentCaptor<GetObjectRequest> getObjectRequest;
-//
-//    @Before
-//    public void setUp() throws IOException {
-//        event = TestUtils.parse("/s3-event.put.json", S3Event.class);
-//
-//        // TODO: customize your mock logic for s3 client
-//        ObjectMetadata objectMetadata = new ObjectMetadata();
-//        objectMetadata.setContentType(CONTENT_TYPE);
-//        when(s3Object.getObjectMetadata()).thenReturn(objectMetadata);
-//        when(s3Client.getObject(getObjectRequest.capture())).thenReturn(s3Object);
-//    }
-//
-//    private Context createContext() {
-//        TestContext ctx = new TestContext();
-//
-//        // TODO: customize your context here if needed.
-//        ctx.setFunctionName("Your Function Name");
-//
-//        return ctx;
-//    }
-//
-//    @Test
-//    public void testCreatePlaylistHandler() {
-//        CreatePlaylistHandler handler = new CreatePlaylistHandler(s3Client);
-//        Context ctx = createContext();
-//
-//        String output = handler.handleRequest(event, ctx);
-//
-//        // TODO: validate output here if needed.
-//        Assert.assertEquals(CONTENT_TYPE, output);
-//    }
+//@RunWith(MockitoJUnitRunner.class) 
+
+public class CreatePlaylistHandlerTest extends LambdaTest {
+	
+	
+	public void testInput(String incoming, String outgoing) throws IOException {
+		 CreatePlaylistHandler handler = new CreatePlaylistHandler();
+		 CreatePlayListRequest request = new Gson().fromJson(incoming, CreatePlayListRequest.class);
+		 
+		 CreatePlayListResponse response = handler.handleRequest(request, createContext("CreatePlaylist"));
+		
+		 Assert.assertEquals(outgoing, response.response);
+	        Assert.assertEquals(200, response.httpCode);
+	    }
+		 
+		 
+	 @Test
+	 public void TestCreatePlaylist() {
+		 String SAMPLE_INPUT_STRING = "{\"name\": \"Heineman is a god\"}";
+		 String RESULT = "Heineman is a god";
+		 try {
+	        	testInput(SAMPLE_INPUT_STRING, RESULT);
+	        	
+	 }
+		 catch (IOException ioe) {
+			 Assert.fail("Unable to Create Playlist:" + RESULT+ ioe.getMessage());
+		 }
+	 }
+	 
+	 @Test
+	 public void FailTest() {
+		 String SAMPLE_INPUT_STRING = "{\"namef\": \"Heineman is a god\"}";
+		 String RESULT = "Unable to Create Playlist: Heineman is a god";
+		 
+		 
+	 }
 }
+
