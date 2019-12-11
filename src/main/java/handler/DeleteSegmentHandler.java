cff10435e1e7aaaaa20135b8696d2a62636b41e0
vinit <vinit@LAPTOP-V0K7N6A5>
2019-12-11 17:03:19 -0500

    delete handler

--- src/main/java/handler/DeleteSegmentHandler.java
+++ src/main/java/handler/DeleteSegmentHandler.java
@@ -3,6 +3,7 @@ package handler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
 
 import empiricist.database.SegmentsDAO;
 import empiricist.http.DeleteSegmentRequest;
@@ -12,10 +13,29 @@ import empiricist.model.Segment;
 
 public class DeleteSegmentHandler implements RequestHandler<DeleteSegmentRequest,DeleteSegmentResponse>{
 	public LambdaLogger logger = null;
+	
+	private AmazonS3 s3  = null;
+	
+	boolean deleteVideo(String id) throws Exception{
+		if (logger !=null) {logger.log("In Delete Segment"); }
+		
+		SegmentsDAO dao = new SegmentsDAO();
+		Segment exists = dao.getSegment(id);
+		Segment segment = new Segment("id","", "","", false);
+		if (exists !=null) {
+			return dao.deleteSegment(segment);
+			
+		}
+		else {
+			return false;
+		}
+		
+		
+	}
 
 	public DeleteSegmentResponse handleRequest(DeleteSegmentRequest req, Context context) {
 		logger = context.getLogger();
-		logger.log("Loading Java Lambda handler to delete");
+		logger.log("Loading Java Lambda handler to delete segment");
 		
 
 		DeleteSegmentResponse response = null;
@@ -27,13 +47,13 @@ public class DeleteSegmentHandler implements RequestHandler<DeleteSegmentRequest
 		// have one part of its information?
 		Segment seg = new Segment(req.id);
 		try {
-			if (dao.deleteSegment(seg)) {
-				response = new DeleteSegmentResponse(req.id, 200);
+			if (deleteVideo(req.getID())) {
+				response = new DeleteSegmentResponse(req.getID(), 200);
 			} else {
-				response = new DeleteSegmentResponse(req.id, 422, "Unable to delete Segment.");
+				response = new DeleteSegmentResponse(req.id, 422, "Unable to delete Segment." + req.getID());
 			}
 		} catch (Exception e) {
-			response = new DeleteSegmentResponse(req.id, 403, "Unable to delete Segment: " + req.id + "(" + e.getMessage() + ")");
+			response = new DeleteSegmentResponse(req.id, 403, "Unable to delete Segment: " + req.getID() + "(" + e.getMessage() + ")");
 		}
 
 		return response;
