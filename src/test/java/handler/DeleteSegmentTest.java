7bef24065b17dfebada9eb02f73fd59da188dea7
vinit <vinit@LAPTOP-V0K7N6A5>
2019-12-12 13:55:55 -0500

    bhjbh

--- src/test/java/handler/DeleteSegmentTest.java
+++ src/test/java/handler/DeleteSegmentTest.java
@@ -27,26 +27,16 @@ public class DeleteSegmentTest extends LambdaTest {
 		DeleteSegmentRequest request = new Gson().fromJson(incoming, DeleteSegmentRequest.class);
 		DeleteSegmentResponse response = handler.handleRequest(request, createContext(null));
 		//
-		Assert.assertEquals(outgoing, response.name);
+		Assert.assertEquals(outgoing, response.id);
         Assert.assertEquals(200, response.statusCode);
     }
 
-public void testInput(String incoming, String outgoing) throws IOException {
-	 UploadSegmentHandler handler = new UploadSegmentHandler();
-	 UploadSegmentRequest request = new Gson().fromJson(incoming, UploadSegmentRequest.class);
-	 
-	 UploadSegmentResponse response = handler.handleRequest(request, createContext(null));
-	
-	 Assert.assertEquals(outgoing, response.response);
-       Assert.assertEquals(422, response.httpCode);
-   }
 
-	
 	
 	 @Test
 	 public void TestDeleteSegment() throws IOException {
-		 String SAMPLE_INPUT_STRING = "{\"id\": \"Test\"}";
-		 String RESULT = "Test"; //
+		 String SAMPLE_INPUT_STRING = "{\"id\": \"UhuraAll c\"}";
+		 String RESULT = "UhuraAll c"; //
 		 try {
 	        	
 	        	TestDeleteInput(SAMPLE_INPUT_STRING,RESULT);
